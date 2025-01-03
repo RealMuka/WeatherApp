119aca62ebd3b76a3d6bbad635f216f1514b9d45
RealMuka <anndreyfedosuk66@gmail.com>
2025-01-03 22:31:25 +0300

    commit

---
+++ GetWeather/ApiResponce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GetWeather
+{
+	public class ApiResponce
+	{
+		[JsonPropertyName("name")] public string Name { get; set; }
+
+		[JsonPropertyName("main")] public MainInfo Main { get; set; }
+	}
+
+	public class MainInfo
+	{
+		[JsonPropertyName("temp_min")] public float MinTemp { get; set; }
+
+		[JsonPropertyName("temp")] public float Temp { get; set; }
+
+		[JsonPropertyName("feels_like")] public float FeelingTemp { get; set; }
+
+		[JsonPropertyName("temp_max")] public float MaxTemp { get; set; }
+	}
+}
---
+++ GetWeather/WeatherGet.cs
@@ -0,0 +1,50 @@
+﻿using GetWeather;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class WeatherGet {
+
+	static async Task Main()
+	{
+		string city;
+
+		city = Console.ReadLine();
+
+		if (string.IsNullOrWhiteSpace(city))
+		{
+			city = "London";
+			Console.WriteLine("Automaticly taked city is London");
+		}
+
+		string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=c0cfb8afcec18b5aa1c71d6dbb0e2b98&units=metric";
+
+		using HttpClient client = new HttpClient();
+
+		try
+		{
+			HttpResponseMessage response = await client.GetAsync(url);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				var result = await response.Content.ReadAsStringAsync();
+
+				var weatherData = JsonSerializer.Deserialize<ApiResponce>(result);
+
+				if (weatherData != null)
+				{
+					Console.WriteLine($"City name: {weatherData.Name}\n" +
+						$"Min temperature: {weatherData.Main.MinTemp}\n" +
+						$"Current temperature: {weatherData.Main.Temp}\n" +
+						$"Feeling temperature: {weatherData.Main.FeelingTemp}\n" +
+						$"Max temperature: {weatherData.Main.MaxTemp}");
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Somthing wrong... ({ex.Message})");
+		}
+	}
+}
+
